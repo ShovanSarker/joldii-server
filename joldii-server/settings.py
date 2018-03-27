74a93ea64dd340e215f97dce513970b2f77ff525
Shovan Sarker <exorcist.shovan@gmail.com>
2018-03-27 13:24:16 +0600

    updated settings for mysql

--- joldii-server/settings.py
+++ joldii-server/settings.py
@@ -82,10 +82,15 @@ WSGI_APPLICATION = 'joldii-server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'core_chadergari',
+        'USER': 'root',
+        'PASSWORD': 'B@Dp@ssw0rd',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
