b3304b7567275262649ba0967c6b9a682836ee1b
Shovan Sarker <exorcist.shovan@gmail.com>
2018-03-27 13:39:50 +0600

    revert db

--- joldii-server/settings.py
+++ joldii-server/settings.py
@@ -82,15 +82,10 @@ WSGI_APPLICATION = 'joldii-server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'core_chadergari',
-        'USER': 'root',
-        'PASSWORD': 'B@Dp@ssw0rd',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
