24d2302bab62ec0c2c38108542c6536d752c44b1
Shovan Sarker <exorcist.shovan@gmail.com>
2018-03-27 15:31:28 +0600

    New response!

---
+++ joldii/responses/common_response.py
@@ -0,0 +1,21 @@
+from base_response import Response
+from joldii.models import SessionModel
+from ..constants import consts
+
+
+class CommonResponse(Response):
+    """
+    Response object for all requests
+    """
+
+    def __init__(self, success=True, reason='', data='', error_code=2000):
+
+        if success:
+            self.status = Response.STATE_SUCCESS
+        else:
+            self.status = Response.STATE_FAIL
+
+        self.response = {}
+        self.set_response(consts.REASON, reason)
+        self.set_response(consts.DATA, data)
+        self.set_response(consts.ERROR_CODE, error_code)
