b9ebfdc6ab41b76b51be1ad006f74a5ffaf0af68
Shovan Sarker <exorcist.shovan@gmail.com>
2018-03-27 14:49:43 +0600

    test url added

--- joldii/urls.py
+++ joldii/urls.py
@@ -6,6 +6,8 @@ from joldii.views import UploadLocation
 from joldii.views import AddVehicle
 from joldii.views import GetRideInformation
 
+from joldii.views import Users
+
 urlpatterns = [
     url(r'^register', view=Register.as_view(), name="register"),
     url(r'^login', view=Login.as_view(), name="login"),
@@ -16,4 +18,7 @@ urlpatterns = [
     url(r'^find_drivers', view=UploadLocation.as_view(), name="find_drivers"),
     url(r'^add_vehicle', view=AddVehicle.as_view(), name="add_vehicle"),
     url(r'^get_ride_info', view=GetRideInformation.as_view(), name="get_ride_info"),
+
+
+    url(r'^test/users', view=Users.as_view(), name="test_users"),
 ]
--- joldii/views/__init__.py
+++ joldii/views/__init__.py
@@ -4,9 +4,12 @@ from joldii.views.session import Register
 from joldii.views.vehicle import AddVehicle
 from joldii.views.map import GetRideInformation
 
+from joldii.views.test import Users
+
 __all__ = ["Register",
            "Login",
            "UploadLocation",
            "AddVehicle",
-           "GetRideInformation"
+           "GetRideInformation",
+           "Users"
            ]
