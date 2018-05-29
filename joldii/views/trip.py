259ce5305625efebfddc8784fb94730676614071
Shovan Sarker <exorcist.shovan@gmail.com>
2018-05-29 01:04:45 -0700

    Search ride debug

--- joldii/views/trip.py
+++ joldii/views/trip.py
@@ -38,12 +38,63 @@ class SearchRide(View):
                                                       reason='Invalid Vehicle Type',
                                                       error_code=consts.ERROR_INCORRECT_VEHICLE_TYPE)
             return HttpResponse(response.respond(), content_type="application/json")
-        try:
-            if RideModel.objects.filter(user=user, order_status__lte=2).exists():
-                previous_incomplete_ride = RideModel.objects.filter(user=user, order_status__lte=2)[0]
+        # try:
+        if RideModel.objects.filter(user=user, order_status__lte=2).exists():
+            previous_incomplete_ride = RideModel.objects.filter(user=user, order_status__lte=2)[0]
+            driver_information = {
+                'driver_name': previous_incomplete_ride.driver.user.username,
+                'driver_phone': previous_incomplete_ride.driver.user.phone
+                # 'driver_rating': selected_driver.driver_profile.average_rating,
+                # 'driver_trip_count': selected_driver.driver_profile.number_of_rides
+            }
+            vehicle_information = {
+                # 'model': selected_driver.current_vehicle.model,
+                # 'registration_number': selected_driver.current_vehicle.registration_number
+            }
+
+            order_data = {
+                'trip_order_id': previous_incomplete_ride.ride_id,
+                'driver_information': driver_information,
+                'vehicle_information': vehicle_information
+            }
+            response = common_response.CommonResponse(success=True,
+                                                      reason='Order Data Retrieved',
+                                                      data=order_data,
+                                                      error_code=consts.ERROR_NONE)
+            return HttpResponse(response.respond(), content_type="application/json")
+        else:
+            print "Finding new ride"
+            new_order = RideModel(
+                user=user,
+                vehicle_class=vehicle_class,
+                pickup_lat=request.POST[consts.PARAM_LAT_FROM],
+                pickup_lon=request.POST[consts.PARAM_LNG_FROM],
+                drop_lat=request.POST[consts.PARAM_LAT_TO],
+                drop_lon=request.POST[consts.PARAM_LNG_FROM],
+                order_status=consts.STATUS_ORDER_PLACED
+            )
+            new_order.save()
+            print "Finding Drivers"
+            selected_driver = SearchRide.find_driver(request.POST[consts.PARAM_LAT_FROM],
+                                                     request.POST[consts.PARAM_LNG_FROM])
+            if selected_driver is None:
+                new_order.order_status = consts.STATUS_ORDER_NO_DRIVER_FOUND
+                new_order.save()
+                response = common_response.CommonResponse(success=False,
+                                                          reason='No Driver Found',
+                                                          error_code=consts.ERROR_NO_DRIVER_FOUND)
+                return HttpResponse(response.respond(), content_type="application/json")
+            else:
+                print()
+                new_order.driver = selected_driver.driver_profile
+                # new_order.vehicle = selected_driver.current_vehicle
+                new_order.order_status = consts.STATUS_ORDER_CONFIRMED
+                new_order.save()
+                selected_driver.driver_status = consts.STATUS_DRIVER_APPROACHING_PICKUP
+                selected_driver.save()
                 driver_information = {
-                    'driver_name': previous_incomplete_ride.driver.user.username,
-                    'driver_phone': previous_incomplete_ride.driver.user.phone
+                    'driver_name': selected_driver.user.username,
+                    'driver_phone': selected_driver.user.phone
                     # 'driver_rating': selected_driver.driver_profile.average_rating,
                     # 'driver_trip_count': selected_driver.driver_profile.number_of_rides
                 }
@@ -53,70 +104,19 @@ class SearchRide(View):
                 }
 
                 order_data = {
-                    'trip_order_id': previous_incomplete_ride.ride_id,
+                    'trip_order_id': new_order.ride_id,
                     'driver_information': driver_information,
                     'vehicle_information': vehicle_information
                 }
                 response = common_response.CommonResponse(success=True,
-                                                          reason='Order Data Retrieved',
+                                                          reason='Order Successfully Received',
                                                           data=order_data,
                                                           error_code=consts.ERROR_NONE)
                 return HttpResponse(response.respond(), content_type="application/json")
-            else:
-                print "Finding new ride"
-                new_order = RideModel(
-                    user=user,
-                    vehicle_class=vehicle_class,
-                    pickup_lat=request.POST[consts.PARAM_LAT_FROM],
-                    pickup_lon=request.POST[consts.PARAM_LNG_FROM],
-                    drop_lat=request.POST[consts.PARAM_LAT_TO],
-                    drop_lon=request.POST[consts.PARAM_LNG_FROM],
-                    order_status=consts.STATUS_ORDER_PLACED
-                )
-                new_order.save()
-                print "Finding Drivers"
-                selected_driver = SearchRide.find_driver(request.POST[consts.PARAM_LAT_FROM],
-                                                         request.POST[consts.PARAM_LNG_FROM])
-                if selected_driver is None:
-                    new_order.order_status = consts.STATUS_ORDER_NO_DRIVER_FOUND
-                    new_order.save()
-                    response = common_response.CommonResponse(success=False,
-                                                              reason='No Driver Found',
-                                                              error_code=consts.ERROR_NO_DRIVER_FOUND)
-                    return HttpResponse(response.respond(), content_type="application/json")
-                else:
-                    print()
-                    new_order.driver = selected_driver.driver_profile
-                    # new_order.vehicle = selected_driver.current_vehicle
-                    new_order.order_status = consts.STATUS_ORDER_CONFIRMED
-                    new_order.save()
-                    selected_driver.driver_status = consts.STATUS_DRIVER_APPROACHING_PICKUP
-                    selected_driver.save()
-                    driver_information = {
-                        'driver_name': selected_driver.user.username,
-                        'driver_phone': selected_driver.user.phone
-                        # 'driver_rating': selected_driver.driver_profile.average_rating,
-                        # 'driver_trip_count': selected_driver.driver_profile.number_of_rides
-                    }
-                    vehicle_information = {
-                        # 'model': selected_driver.current_vehicle.model,
-                        # 'registration_number': selected_driver.current_vehicle.registration_number
-                    }
-
-                    order_data = {
-                        'trip_order_id': new_order.ride_id,
-                        'driver_information': driver_information,
-                        'vehicle_information': vehicle_information
-                    }
-                    response = common_response.CommonResponse(success=True,
-                                                              reason='Order Successfully Received',
-                                                              data=order_data,
-                                                              error_code=consts.ERROR_NONE)
-                    return HttpResponse(response.respond(), content_type="application/json")
-        except:
-            response = common_response.CommonResponse(success=False,
-                                                      reason='Incorrect Parameters',
-                                                      error_code=consts.ERROR_INCORRECT_PARAMETERS)
+        # except:
+        #     response = common_response.CommonResponse(success=False,
+        #                                               reason='Incorrect Parameters',
+        #                                               error_code=consts.ERROR_INCORRECT_PARAMETERS)
             return HttpResponse(response.respond(), content_type="application/json")
 
     @staticmethod
